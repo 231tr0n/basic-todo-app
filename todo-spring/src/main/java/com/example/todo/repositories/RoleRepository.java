0241ce199225af95ba7231a515bbf1fd3bd4737f
231tr0n <zeltronsrikar@gmail.com>
2025-12-09 23:48:16 +0530

    test

---
+++ todo-spring/src/main/java/com/example/todo/repositories/RoleRepository.java
@@ -0,0 +1,6 @@
+package com.example.todo.repositories;
+
+import com.example.todo.entities.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {}
