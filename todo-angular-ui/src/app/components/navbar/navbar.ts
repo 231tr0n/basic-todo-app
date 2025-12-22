9d8f6bf7f2b01ca3a678dea006e8928c610683e6
231tr0n <zeltronsrikar@gmail.com>
2025-12-23 00:27:26 +0530

    test

--- todo-angular-ui/src/app/components/navbar/navbar.ts
+++ todo-angular-ui/src/app/components/navbar/navbar.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatButton } from '@angular/material/button';
+import { MatMiniFabButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
 	selector: 'app-navbar',
-	imports: [MatIcon, MatButton, MatToolbar],
+	imports: [MatIcon, MatMiniFabButton, MatToolbar],
 	templateUrl: './navbar.html',
 	styleUrl: './navbar.css',
 	changeDetection: ChangeDetectionStrategy.OnPush
--- todo-angular-ui/src/app/interceptors/error-interceptor.ts
+++ todo-angular-ui/src/app/interceptors/error-interceptor.ts
@@ -3,9 +3,8 @@ import { inject } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 import { Session } from '../services/session';
 
-const session = inject(Session);
-
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+	const session = inject(Session);
 	return next(req).pipe(
 		catchError((err: HttpErrorResponse) => {
 			if (err.status === HttpStatusCode.Unauthorized.valueOf()) {
