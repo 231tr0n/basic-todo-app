5a9cfa05627ce761881e26cfaf755cc37b704f98
231tr0n <zeltronsrikar@gmail.com>
2025-12-22 16:42:34 +0530

    test

--- todo-angular-ui/src/app/services/global-service.spec.ts
+++ todo-angular-ui/src/app/services/global-service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { GlobalService } from './global-service';
 
-describe('Todo', () => {
+describe('GlobalService', () => {
 	let service: GlobalService;
 
 	beforeEach(() => {
